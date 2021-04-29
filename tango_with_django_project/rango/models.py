ed32bfd3774da76e2e3559bc7a14edd2cf868b57
MarcelloGreen95 <69159494+MarcelloGreen95@users.noreply.github.com>
2021-04-29 12:54:10 +0100

    Chapter 6 + attempt fixing issues from chapter 5

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -6,6 +6,10 @@ class Category(models.Model):
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -21,5 +25,3 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
-
-
